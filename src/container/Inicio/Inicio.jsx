ddeba06e5b6cd2010ac6422280b9f15695eac5e3
rogervalentim <valentim.roger33@gmail.com>
2023-01-06 21:51:15 -0300

    adicionando um type animation no inicio

--- src/container/Inicio/Inicio.jsx
+++ src/container/Inicio/Inicio.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Inicio.css";
 import Avatar from "../../assets/images/inicio/avatar.png";
 import Curriculum from "../../assets/images/inicio/curriculo-Roger-Valentim.pdf";
+import { TypeAnimation } from "react-type-animation";
 
 function Inicio() {
   return (
@@ -9,21 +10,27 @@ function Inicio() {
       <section className="home-container" id="home">
         <div className="container-title">
           <h3 className="home-title">Olá eu sou o Roger,</h3>
-          <h3 className="text-animation">
-            Desenvolvedor Web
-            <br></br>Full Stack !
-          </h3>
+          <TypeAnimation
+            sequence={[
+              "Desenvolvedor Web", 
+              1000,
+              "Full Stack !",
+              2000,
+            ]}
+            wrapper="div"
+            cursor={true}
+            repeat={Infinity}
+            className="text-animation"
+
+          />
           <a href={Curriculum} target="_blank">
-            <button className="link-btn">
-            Baixar CV
-            </button>
-            </a>
+            <button className="link-btn">Baixar CV</button>
+          </a>
         </div>
         <div className="avatar-container">
           <img className="avatar" src={Avatar} alt="avatar inicio" />
         </div>
       </section>
-
     </>
   );
 }
