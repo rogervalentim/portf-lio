383102feba68e87f5a0d6b3a326cca35777eb8c0
rogervalentim <valentim.roger33@gmail.com>
2023-03-14 15:34:13 -0300

    retaforando a seção de tecnologias

--- src/constants/data.js
+++ src/constants/data.js
@@ -7,6 +7,68 @@ const about = [
     },
 ]
 
+const tecnologies = [
+  {
+    imagem:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+    name: "Html",
+  },
+
+  {
+    imagem:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+    name: "Css"
+  },
+
+  {
+    imagem:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+    name: "Tailwindcss"
+  },
+
+  {
+    imagem:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    name: "Javascript"
+  },
+
+  {
+    imagem:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+    name: "Typescript"
+  },
+
+  {
+    imagem:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    name: "Git"
+  },
+
+  {
+    imagem:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+    name: "Node"
+  },
+
+  {
+    imagem:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg",
+    name: "MySql"
+  },
+
+  {
+    imagem:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+    name: "Mongo Db"
+  },
+
+  {
+    imagem:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    name: "React"
+  }
+];
+
 const projects = [
     {
       image: images.RocketPay,
@@ -73,4 +135,4 @@ const projects = [
   
   ];
 
-  export default { projects, about }
+  export default { projects, about, tecnologies }
--- src/container/Tecnologias/Tecnologias.jsx
+++ src/container/Tecnologias/Tecnologias.jsx
@@ -2,76 +2,17 @@ import React from 'react';
 
 import TecnologiasCard from '../../components/Tecnologias/TecnologiasCard';
 
+import data from '../../constants/data';
+
 import './Tecnologias.css';
 
-const tecnologies = [
-  {
-    imagem:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
-    name: "Html",
-  },
-
-  {
-    imagem:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
-    name: "Css"
-  },
-
-  {
-    imagem:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
-    name: "Tailwindcss"
-  },
-
-  {
-    imagem:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
-    name: "Javascript"
-  },
-
-  {
-    imagem:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-    name: "Typescript"
-  },
-
-  {
-    imagem:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
-    name: "Git"
-  },
-
-  {
-    imagem:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
-    name: "Node"
-  },
-
-  {
-    imagem:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg",
-    name: "MySql"
-  },
-
-  {
-    imagem:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
-    name: "Mongo Db"
-  },
-
-  {
-    imagem:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
-    name: "React"
-  }
-];
 
 function Tecnologias() {
   return (
     <section className="technologies-container" id="tecnologies">
       <h1 className="technologies-title">Tecnologias</h1>
       <div className="cards">
-          {tecnologies.map((technologie, i) => (
+          {data.tecnologies.map((technologie, i) => (
               <TecnologiasCard
                 key={i}
                 imagem={technologie.imagem}
